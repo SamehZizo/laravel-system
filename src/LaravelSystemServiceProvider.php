8930d9abfb3f252fa3d6c573eb9e5ee05b9ff2f6
SamehZizo <samh_zamalkawe@hotmail.com>
2021-10-01 14:33:02 +0200

    test 2

--- src/LaravelSystemServiceProvider.php
+++ src/LaravelSystemServiceProvider.php
@@ -9,7 +9,7 @@ class LaravelSystemServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        dd('It works! 22');
+        dd('It works! 33');
     }
 
     public function register()
