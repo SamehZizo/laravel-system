de6a7aa012803d508a3175586cb036b178463cac
SamehZizo <samh_zamalkawe@hotmail.com>
2021-10-02 17:08:04 +0200

    update migrations

---
+++ database/migrations/000002_laravel_system_create_system_forms_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\Schema;
+
+class LaravelSystemCreateSystemFormsTable extends Migration
+{
+
+    /**
+     * The database schema.
+     *
+     * @var Builder
+     */
+    protected $schema;
+
+    /**
+     * Create a new migration instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->schema = Schema::connection($this->getConnection());
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->schema->create('system_forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('title2');
+            $table->string('name')->unique();
+            $table->bigInteger('created_by')->nullable();
+            $table->bigInteger('updated_by')->nullable();
+            $table->bigInteger('deleted_by')->nullable();
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+            $table->dateTime('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $this->schema->dropIfExists('system_forms');
+    }
+}
