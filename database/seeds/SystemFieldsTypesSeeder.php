9be0d44575c09a1f9d824c22f4c26e36473bb9ac
SamehZizo <samh_zamalkawe@hotmail.com>
2021-10-05 21:25:59 +0200

    update seeds

--- database/seeds/SystemFieldsTypesSeeder.php
+++ database/seeds/SystemFieldsTypesSeeder.php
@@ -8,7 +8,7 @@ use Sameh\LaravelSystem\Models\System\FormBuilder\SystemFieldsType;
 
 class SystemFieldsTypesSeeder extends Seeder
 {
-    private $field_types = [
+    private $field_types = array(
         array('id' => 1, 'title' => 'Text Input', 'slug' => 'text-input'),
         array('id' => 2, 'title' => 'Text Editor', 'slug' => 'text-editor'),
         array('id' => 3, 'title' => 'Model Dropdown', 'slug' => 'model-dropdown'),
@@ -17,7 +17,7 @@ class SystemFieldsTypesSeeder extends Seeder
         array('id' => 6, 'title' => 'Dropdown', 'slug' => 'dropdown'),
         array('id' => 7, 'title' => 'Multi Model Dropdown', 'slug' => 'multi-model-dropdown'),
         array('id' => 8, 'title' => 'Date Input', 'slug' => 'date-input'),
-    ];
+    );
 
     public function run()
     {
