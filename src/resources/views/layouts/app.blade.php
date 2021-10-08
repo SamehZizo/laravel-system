a684e5261e0aab53ede08423d6b32411c54b2468
SamehZizo <samh_zamalkawe@hotmail.com>
2021-10-08 11:07:15 +0200

    update view

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,24 +1,24 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    @include('layouts.head')
+    @include('laravel-system:layouts.head')
 </head>
 <body class="system-font">
-@include('layouts.nav.nav')
+@include('laravel-system:layouts.nav.nav')
 <main class="{{--py-4--}} pb-4 col-12">
     @yield('content')
 </main>
 <!--Notices-->
-@include('notices.success')
-@include('notices.danger')
+@include('laravel-system:notices.success')
+@include('laravel-system:notices.danger')
 <!--End Notices-->
 <!--Modals-->
-@include('form_builder.modals.form_builder_modal')
-@include('form_builder.modals.delete')
-@include('form_builder.modals.view')
+@include('laravel-system:form_builder.modals.form_builder_modal')
+@include('laravel-system:form_builder.modals.delete')
+@include('laravel-system:form_builder.modals.view')
 <!--End Modals-->
 <!--Foot-->
-@include('layouts.foot')
+@include('laravel-system:layouts.foot')
 <!--End Foot-->
 </body>
 </html>
