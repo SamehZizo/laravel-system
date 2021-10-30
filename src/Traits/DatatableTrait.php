6fddac69155e9cf7ea5ff38f00ab832173a3d642
SamehZizo <samh_zamalkawe@hotmail.com>
2021-10-30 15:31:23 +0200

    update view to open new tab when click view

--- src/Traits/DatatableTrait.php
+++ src/Traits/DatatableTrait.php
@@ -16,11 +16,11 @@ trait DatatableTrait
 
                 $btn = '<ul class="list-inline mb-0">';
 
-                if ($child) {
-                    /*$view_route = route($this->getRouteName() . '.child_show', ['id' => $row->id]);
+                /*if ($child) {
+                    $view_route = route($this->getRouteName() . '.child_show', ['id' => $row->id]);
                     $edit_route = route($this->getRouteName() . '.child_edit', ['id' => $row->id]);
                     $update_route = route($this->getRouteName() . '.child_update', ['id' => $row->id]);
-                    $delete_route = route($this->getRouteName() . '.child_destroy', ['id' => $row->id]);*/
+                    $delete_route = route($this->getRouteName() . '.child_destroy', ['id' => $row->id]);
 
                     if ($this->getShowViewButton()) {
                         $btn .= $this->get_child_view_button($row);
@@ -33,7 +33,7 @@ trait DatatableTrait
                     if ($this->getShowDeleteButton()) {
                         $btn .= $this->get_child_delete_button($row);
                     }
-                } else {
+                } else {*/
 
                     if ($this->getShowViewButton()) {
                         $btn .= $this->get_view_button($row, $child);
@@ -46,7 +46,7 @@ trait DatatableTrait
                     if ($this->getShowDeleteButton()) {
                         $btn .= $this->get_delete_button($row);
                     }
-                }
+                //}
 
                 $btn .= '</ul>';
 
@@ -61,15 +61,15 @@ trait DatatableTrait
         //$view_route = route($this->getRouteName() . '.show', ['id' => $row->id]);
         $view_route = route($this->getViewRouteName(), ['id' => $row->id]);
 
-        return '<li class="list-inline-item"><a href="#" class="form-builder-modal-button text-success" data-toggle="modal" data-target="#viewModal"
+        /*return '<li class="list-inline-item"><a href="#" class="form-builder-modal-button text-success" data-toggle="modal" data-target="#viewModal"
             data-id="' . $row->id . '" data-title="' . 'View ' . $this->getSingularName() . '" data-form-route="' . $view_route . '"
-            data-backdrop="static" data-keyboard="false"><i class="fa fa-eye"></i></a></li>';
-        /*if ($child) {
+            data-backdrop="static" data-keyboard="false"><i class="fa fa-eye"></i></a></li>';*/
+        if ($child) {
             return '<li class="list-inline-item"><a href="#" class="form-builder-modal-button text-success" data-toggle="modal" data-target="#formBuilderModal"
             data-id="' . $row->id . '" data-title="' . 'View ' . $this->getSingularName() . '" data-form-route="' . $view_route . '"><i class="fa fa-eye"></i></a></li>';
         } else {
             return '<li class="list-inline-item"><a href="' . $view_route . '" class="text-success" target="_blank"><i class="fa fa-eye"></i></a></li>';
-        }*/
+        }
     }
 
     protected function get_edit_button($row): string
@@ -92,7 +92,11 @@ trait DatatableTrait
 
     protected function get_child_view_button($row)
     {
-        return '';
+        $view_route = route($this->getRouteName() . '.child_show', ['id' => $row->id]);
+
+        return '<li class="list-inline-item"><a href="#" class="form-builder-modal-button text-success view-child-button"
+            data-id="' . $row->id . '" data-title="' . 'View ' . $this->getSingularName() . '" data-form-route="' . $view_route . '"
+            data-backdrop="static" data-keyboard="false"><i class="fa fa-eye"></i></a></li>';
     }
 
     protected function get_child_edit_button($row)
--- src/resources/views/layouts/modules/view.blade.php
+++ src/resources/views/layouts/modules/view.blade.php
@@ -1,14 +1,15 @@
-{{--@extends('layouts.app')
-@section('content')--}}
-    {{--<div class="row col-12 mt-2">
+@extends('laravel-system::layouts.app')
+@section('content')
+    <div class="row col-12 mt-2">
         <div class="col-6">
             <h1 class="m-0">{{$record->getSingularName()}} : {{$record->title}}</h1>
-        </div>--}}
-        {{--<div class="col-6">
+        </div>
+        <div class="col-6">
             <h1 class="m-0 pull-right">{{$record->title_ar}}</h1>
-        </div>--}}
-    {{--</div>
-    <hr class="row col-12">--}}
+        </div>
+    </div>
+    <hr class="row col-12">
+    {{--Above open to view toolbar--}}
     <div class="row {{--col-12--}}">
         <div class="col-md-2 col-xs-12 mb-3 {{--p-0--}} view-nav">
             <ul class="nav flex-column view-tabs">
@@ -25,4 +26,4 @@
             @yield('data_section')
         </div>
     </div>
-{{--@endsection--}}
+@endsection
