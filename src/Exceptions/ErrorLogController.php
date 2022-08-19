20f1643960b8012907f38ba8bb10104b076e8c70
Sameh <sameh.mohamed@brokerage-insurance.com>
2022-08-19 18:57:16 +0200

    update error log

--- src/Exceptions/ErrorLogController.php
+++ src/Exceptions/ErrorLogController.php
@@ -10,24 +10,31 @@ class ErrorLogController
 {
     public static function storeErrorLog(Throwable $e, Request $request)
     {
-        $exceptionLog = new ExceptionLog;
-        // throwable
-        $exceptionLog->source_id = null;
-        $exceptionLog->user_id = Auth::user()->id ?? null;
-        $exceptionLog->code = $e->getCode() ?? null;
-        $exceptionLog->file = $e->getFile() ?? null;
-        $exceptionLog->line = $e->getLine() ?? null;
-        $exceptionLog->message = $e->getMessage() ?? null;
-        $exceptionLog->trace = $e->getTraceAsString() ?? null;
-        // request
-        $exceptionLog->body = json_encode($request->all()) ?? null;
-        $exceptionLog->url = $request->url() ?? null;
-        $exceptionLog->agent = $request->userAgent() ?? null;
-        $exceptionLog->root = $request->root() ?? null;
-        $exceptionLog->header = $request->header() ?? null;
-        $exceptionLog->is_json = $request->acceptsJson();
-        $exceptionLog->is_ajax = $request->ajax();
-        // save
-        $exceptionLog->save();
+        try {
+            $exceptionLog = new ExceptionLog;
+            // throwable
+            $exceptionLog->source_id = null;
+            $exceptionLog->user_id = Auth::user()->id ?? null;
+            $exceptionLog->code = $e->getCode() ?? null;
+            $exceptionLog->file = $e->getFile() ?? null;
+            $exceptionLog->line = $e->getLine() ?? null;
+            $exceptionLog->message = $e->getMessage() ?? null;
+            $exceptionLog->trace = $e->getTraceAsString() ?? null;
+            // request
+            $exceptionLog->body = json_encode($request->all()) ?? null;
+            $exceptionLog->url = $request->url() ?? null;
+            $exceptionLog->agent = $request->userAgent() ?? null;
+            $exceptionLog->root = $request->root() ?? null;
+            $exceptionLog->header = $request->header() ?? null;
+            $exceptionLog->is_json = $request->acceptsJson();
+            $exceptionLog->is_ajax = $request->ajax();
+            // save
+            $exceptionLog->save();
+        } catch (\Exception $e) {
+            $exceptionLog = new ExceptionLog;
+            $exceptionLog->message = $e->getMessage();
+            $exceptionLog->save();
+        }
+
     }
 }
